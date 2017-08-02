6ee661b648980e11a8ce41939eea9618fa1127eb
Tom Simnett <tom@initforthe.com>
2017-08-02 11:05:02 +0100

    Add layouts

--- app/controllers/spina/case_studies/case_studies_controller.rb
+++ app/controllers/spina/case_studies/case_studies_controller.rb
@@ -3,10 +3,12 @@ module Spina::CaseStudies
 
     def index
       @case_studies = Spina::CaseStudies::CaseStudy.all
+      render layout: "#{current_theme.name.parameterize.underscore}/application"
     end
 
     def show
       @case_study = Spina::CaseStudies::CaseStudy.find params[:id]
+      render layout: "#{current_theme.name.parameterize.underscore}/application"
     end
   end
 end
