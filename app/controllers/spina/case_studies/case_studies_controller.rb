244b4c1c6712a02c2b7b142cccea58b790c6cb61
Tom Simnett <tom@initforthe.com>
2017-08-02 11:33:43 +0100

    Fix form for positioning and image selection

--- app/controllers/spina/case_studies/case_studies_controller.rb
+++ app/controllers/spina/case_studies/case_studies_controller.rb
@@ -1,5 +1,6 @@
 module Spina::CaseStudies
   class CaseStudiesController < ::Spina::ApplicationController
+    before_action :set_page
 
     def index
       @case_studies = Spina::CaseStudies::CaseStudy.all
@@ -9,6 +10,17 @@ module Spina::CaseStudies
     def show
       @case_study = Spina::CaseStudies::CaseStudy.find params[:id]
       render layout: "#{current_theme.name.parameterize.underscore}/application"
+    end
+
+    private
+
+    def set_page
+      @page = Spina::Page.find_or_create_by name: 'case_studies' do |page|
+        page.link_url = '/case_studies'
+        page.title = 'Case Studies'
+        page.view_template = 'show'
+        page.deletable = false
+      end
     end
   end
 end
