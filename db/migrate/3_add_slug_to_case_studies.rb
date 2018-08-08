e166bcd89acf22f948d4ba061155fe4584f82437
Tomislav Simnett <tom@initforthe.com>
2018-08-08 16:47:48 +0100

    Fix migration class name

--- db/migrate/3_add_slug_to_case_studies.rb
+++ db/migrate/3_add_slug_to_case_studies.rb
@@ -1,4 +1,4 @@
-class CreateSpinaCaseStudiesTestimonials < ActiveRecord::Migration[5.2]
+class AddSlugToCaseStudies < ActiveRecord::Migration[5.2]
   def change
     add_column :spina_case_studies_case_studies, :slug, :string, index: true
   end
